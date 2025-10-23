2171794d72ac3fe550d314b74262c99b88b048e3
Infernario1130 <infernario11@gmail.com>
2025-10-23 23:42:34 +0530

    feat: Add loaders,and filter modals.

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import LoginModal from "./components/modals/LoginModal";
 import RentModal from "./components/modals/RentModal";
 import "leaflet/dist/leaflet.css";
 import ClientOnly from "./components/ClientOnly";
+import SearchModal from "./components/modals/SearchModal";
 
 const font =  Nunito ({
   variable: "--font-nunito",
@@ -37,6 +38,7 @@ export default async function RootLayout({
         className={`${font.variable} antialiased`}
       >
         <ToasterProvider/>
+        <SearchModal/>
         <RentModal/>
         <RegisterModal/>
         <LoginModal/>
